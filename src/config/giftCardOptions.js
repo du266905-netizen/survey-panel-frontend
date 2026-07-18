abf7cf418ec672ff48f75b51c1b6adcd25ae4530
du266905-netizen <du266905@gmail.com>
2026-07-18 15:58:46 +0800

    Use clean transparent gift card assets

--- src/config/giftCardOptions.js
+++ src/config/giftCardOptions.js
@@ -1,30 +1,30 @@
-import amazonImage from '../assets/giftcards/amazon.jpg';
-import appleImage from '../assets/giftcards/apple.jpg';
-import googlePlayImage from '../assets/giftcards/googleplay.jpg';
-import grubhubImage from '../assets/giftcards/grubhub.jpg';
-import instacartImage from '../assets/giftcards/instacart.jpg';
-import lowesImage from '../assets/giftcards/lowes.jpg';
-import starbucksImage from '../assets/giftcards/starbucks.jpg';
-import uberImage from '../assets/giftcards/uber.jpg';
+import amazonImage from '../assets/giftcards/amazon.png';
+import appleImage from '../assets/giftcards/apple.png';
+import googlePlayImage from '../assets/giftcards/googleplay.png';
+import grubhubImage from '../assets/giftcards/grubhub.png';
+import instacartImage from '../assets/giftcards/instacart.png';
+import lowesImage from '../assets/giftcards/lowes.png';
+import starbucksImage from '../assets/giftcards/starbucks.png';
+import uberImage from '../assets/giftcards/uber.png';
 
 export const giftCardOptions = [
-  { id: 'amazon', name: 'Amazon', image: 'amazon.jpg', region: 'global', amounts: [10, 25, 50] },
-  { id: 'apple', name: 'Apple', image: 'apple.jpg', region: 'global', amounts: [10, 25, 50] },
-  { id: 'googleplay', name: 'Google Play', image: 'googleplay.jpg', region: 'global', amounts: [10, 25, 50] },
-  { id: 'lowes', name: "Lowe's", image: 'lowes.jpg', region: 'global', amounts: [10, 25, 50] },
-  { id: 'starbucks', name: 'Starbucks', image: 'starbucks.jpg', region: 'global', amounts: [10, 25, 50] },
-  { id: 'grubhub', name: 'Grubhub', image: 'grubhub.jpg', region: 'global', amounts: [25, 50, 75] },
-  { id: 'uber', name: 'Uber', image: 'uber.jpg', region: 'global', amounts: [25, 50, 75] },
-  { id: 'instacart', name: 'Instacart', image: 'instacart.jpg', region: 'global', amounts: [25, 50, 75] },
+  { id: 'amazon', name: 'Amazon', image: 'amazon.png', region: 'global', amounts: [10, 25, 50] },
+  { id: 'apple', name: 'Apple', image: 'apple.png', region: 'global', amounts: [10, 25, 50] },
+  { id: 'googleplay', name: 'Google Play', image: 'googleplay.png', region: 'global', amounts: [10, 25, 50] },
+  { id: 'lowes', name: "Lowe's", image: 'lowes.png', region: 'global', amounts: [10, 25, 50] },
+  { id: 'starbucks', name: 'Starbucks', image: 'starbucks.png', region: 'global', amounts: [10, 25, 50] },
+  { id: 'grubhub', name: 'Grubhub', image: 'grubhub.png', region: 'global', amounts: [25, 50, 75] },
+  { id: 'uber', name: 'Uber', image: 'uber.png', region: 'global', amounts: [25, 50, 75] },
+  { id: 'instacart', name: 'Instacart', image: 'instacart.png', region: 'global', amounts: [25, 50, 75] },
 ];
 
 export const giftCardImageSources = {
-  'amazon.jpg': amazonImage,
-  'apple.jpg': appleImage,
-  'googleplay.jpg': googlePlayImage,
-  'grubhub.jpg': grubhubImage,
-  'instacart.jpg': instacartImage,
-  'lowes.jpg': lowesImage,
-  'starbucks.jpg': starbucksImage,
-  'uber.jpg': uberImage,
+  'amazon.png': amazonImage,
+  'apple.png': appleImage,
+  'googleplay.png': googlePlayImage,
+  'grubhub.png': grubhubImage,
+  'instacart.png': instacartImage,
+  'lowes.png': lowesImage,
+  'starbucks.png': starbucksImage,
+  'uber.png': uberImage,
 };
